0b62f3153984428509123493a484123301e2dbec
arosado1998 <arosado1998@gmail.com>
2022-08-18 16:08:46 -0400

    Configure React app for deployment to GitHub Pages

---
+++ src/About/About.jsx
@@ -0,0 +1,17 @@
+import './About.css';
+
+const About = () => {
+    return (
+      <div className="About">
+        <header className="About-header">
+          <p>
+            Start of some shit
+          </p>
+         
+        </header>
+      </div>
+    );
+  };
+  
+  export default About;
+  
